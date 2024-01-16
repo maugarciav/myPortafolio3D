c052904e2a23c0cf13ffe10a44704aa1683c1e60
maugarciav <mau.villanueva2002@gmail.com>
2024-01-16 17:21:37 -0600

    hero: add mobile view to hero

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -7,8 +7,8 @@ const Loader = () => {
   return (
     <Html>
       <div className="flex items-center justify-center h-full">
-        <span className="animate-spin h-20 w-20 border-t-2 border-forthy border-solid rounded-full"></span>
-        <p className="text-forthy text-md  ml-2 -translate-x-16">{progress.toFixed()}%</p>  
+        <span className="animate-spin h-20 w-20 border-t-2 border-white border-solid rounded-full"></span>
+        <p className="text-white text-md  ml-2 -translate-x-16">{progress.toFixed()}%</p>  
       </div>
     </Html>
   );
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,19 +1,19 @@
-import React, { Suspense, useEffect, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ mobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
-  // const computer = useGLTF('./car/Car_cartoon.gltf')
+
   return (
     <mesh>
       <hemisphereLight intensity={5} groundColor="black" />
       <pointLight intensity={5} />
       <primitive 
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={mobile ? 0.4 : 0.75}
+        position={mobile ? [0, -2.5, -0.75] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.01]} 
       />
     </mesh>
@@ -21,6 +21,23 @@ const Computers = () => {
 }
 
 const ComputerCanvas = () => {
+  const [mobile, setMobil] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+    setMobil(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setMobil(event.matches)
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    return() => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
   return(
     <Canvas
       frameloop='demand'
@@ -34,7 +51,7 @@ const ComputerCanvas = () => {
         maxPolarAngle={Math.PI/2}
         minPolarAngle={Math.PI/2}
         />
-        <Computers/>
+        <Computers mobile={mobile}/>
       </Suspense>
       <Preload all />
     </Canvas>

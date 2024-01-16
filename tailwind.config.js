b176b482e3753a7811fcb4b9c8ed21bc9a4aab93
maugarciav <mau.villanueva2002@gmail.com>
2024-01-15 19:30:13 -0600

    add hero information

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default  {
         primary: "#050816",
         secondary: "#aaa6c3",
         tertiary: "#151030",
+        forthy: "#915eff",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",

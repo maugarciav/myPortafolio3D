4f2d36ba8615993c52548c0fd71566f0abe7fc4c
maugarciav <mau.villanueva2002@gmail.com>
2024-01-16 17:54:15 -0600

    about: add about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
         <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center item-center'>
           <a href='#about'>
             <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-              <motion.dev className='w-3 h-3 rounded-full bg-secondary mb-1'
+              <motion.div className='w-3 h-3 rounded-full bg-secondary mb-1'
                 animate={{
                   y: [0, 24, 0]
                 }}

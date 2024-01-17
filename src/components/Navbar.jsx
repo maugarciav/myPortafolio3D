af901f2e08f4422c197d608558a9bef86dd819e5
maugarciav <mau.villanueva2002@gmail.com>
2024-01-17 16:25:48 -0600

    bug: navbar fix bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [toggle, setToogle] = useState(false)
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20' bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center mx-auto'>
         <Link 
           to='/'

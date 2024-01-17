661cea8c9e0d9ae115ec8ea4c57e237ecef6e430
maugarciav <mau.villanueva2002@gmail.com>
2024-01-17 16:56:08 -0600

    add: balls tech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BallCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { technologies } from '../constants'
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex justify-center gap-10 flex-wrap'>
+      {technologies.map(technologie => (
+        <div 
+          key={technologie.name}
+          className='w-28 h-28'>
+            <BallCanvas icon={technologie.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+const TechSectionWrapper = SectionWrapper(Tech, '')
+export default TechSectionWrapper
+

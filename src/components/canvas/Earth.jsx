b0d3def51f175ad12d4d2630cc3223420cb29248
maugarciav <mau.villanueva2002@gmail.com>
2024-01-19 14:01:29 -0600

    hotfix: no comments

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -3,18 +3,13 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
-
 const Earth = () => {
   const earth = useGLTF('./planet/scene.gltf')
-
 
   return (
     <primitive
       object={earth.scene}
       scale={2.5}
-      // position-y={0}
-      // rotation-y={0}
-
     />
   )
 }
@@ -27,8 +22,6 @@ const EarthCanvas = () => {
       gl={{preserveDrawingBuffer: true}}
       camera={{
         fov:45,
-        // near: 0.1,
-        // far: 200,
         position: [-4,3,6]
       }}
     >
@@ -40,7 +33,6 @@ const EarthCanvas = () => {
           maxPolarAngle={Math.PI/2}
           minPolarAngle={Math.PI/2}
         />
-
         <Earth/>
       </Suspense>
 
